22ffba57d533bb33bc0755221602d9ef5174e5d2
João Pedro <joaopedrosanchez302@gmail.com>
2020-11-05 16:48:25 -0300

    Created debounce and helpers.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
 import DarkMode from './modules/mode';
 new DarkMode('[data-mode="switch"]', 'dark-mode', '[data-mode="dark"]');
 
-import dropdown from './modules/dropdown';
-import initEvents from './modules/fetchCountries';
-import callDetails from './modules/detailsFetch';
+// import dropdown from './modules/dropdown';
+// import callDetails from './modules/detailsFetch';
 
-dropdown();
-initEvents();
-callDetails();
+// dropdown();
+// callDetails();
---
+++ src/js/modules/apiFetch.js
@@ -0,0 +1,5 @@
+export default class ApiFetch {
+  constructor(url) {
+    this.url = url;
+  }
+}
